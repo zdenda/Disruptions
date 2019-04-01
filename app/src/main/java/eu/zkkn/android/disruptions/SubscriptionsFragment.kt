e8113e6eeb8bfc568b0cf7d682fb6704108ddcb2
Zdeněk Kořán <zkoran@gmail.com>
2019-04-01 18:31:27 +0200

    add more colors

--- app/src/main/java/eu/zkkn/android/disruptions/SubscriptionsFragment.kt
+++ app/src/main/java/eu/zkkn/android/disruptions/SubscriptionsFragment.kt
@@ -1,13 +1,17 @@
 package eu.zkkn.android.disruptions
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EdgeEffect
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.RecyclerView
 import eu.zkkn.android.disruptions.data.Subscription
 import kotlinx.android.synthetic.main.fragment_subscriptions.*
 
@@ -35,7 +39,30 @@ class SubscriptionsFragment : Fragment() {
         adapter.setOnRemoveClickListener { lineName ->
             viewModel.removeSubscription(lineName)
         }
+        adapter.registerAdapterDataObserver(object : RecyclerView.AdapterDataObserver() {
+            override fun onItemRangeInserted(positionStart: Int, itemCount: Int) {
+                rwSubscriptions.scrollToPosition(positionStart)
+            }
+        })
+
+
+        // change color for scroll edge effect
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            rwSubscriptions.edgeEffectFactory = object : RecyclerView.EdgeEffectFactory() {
+                override fun createEdgeEffect(view: RecyclerView, direction: Int): EdgeEffect {
+                    val context = view.context
+                    return EdgeEffect(context).apply {
+                        color = if (direction == DIRECTION_BOTTOM) {
+                            ContextCompat.getColor(context, R.color.backgroundSecondary)
+                        } else {
+                            ContextCompat.getColor(context, R.color.colorPrimary)
+                        }
+                    }
+                }
+            }
+        }
         rwSubscriptions.adapter = adapter
+        rwSubscriptions.setHasFixedSize(true)
         rwSubscriptions.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
         viewModel.subscriptions.observe(viewLifecycleOwner, Observer<List<Subscription>> { subscriptions ->
             empty.visibility = if (subscriptions.isEmpty()) View.VISIBLE else View.GONE
