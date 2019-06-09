ea57a70372e91026fa36d6c0688824d3fda089b7
Zdeněk Kořán <zkoran@gmail.com>
2019-06-09 17:59:29 +0200

    upgrade dependencies

--- app/src/main/java/eu/zkkn/android/disruptions/utils/Executors.kt
+++ app/src/main/java/eu/zkkn/android/disruptions/utils/Executors.kt
@@ -1,7 +1,15 @@
 package eu.zkkn.android.disruptions.utils
 
 import java.util.concurrent.Executors
+import java.util.concurrent.LinkedBlockingQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
 
+
+private val NUMBER_OF_CORES = Runtime.getRuntime().availableProcessors()
+
+private val BACKGROUND_WORK_EXECUTOR = ThreadPoolExecutor(NUMBER_OF_CORES, NUMBER_OF_CORES,
+    1L, TimeUnit.SECONDS, LinkedBlockingQueue<Runnable>())
 
 private val IO_EXECUTOR = Executors.newSingleThreadExecutor()
 
@@ -11,4 +19,11 @@ private val IO_EXECUTOR = Executors.newSingleThreadExecutor()
  */
 fun ioThread(f : () -> Unit) {
     IO_EXECUTOR.execute(f)
+}
+
+/**
+ * Utility method to run blocks on a dedicated threads for background work.
+ */
+fun backgroundThread(f: () -> Unit) {
+    BACKGROUND_WORK_EXECUTOR.execute(f)
 }
