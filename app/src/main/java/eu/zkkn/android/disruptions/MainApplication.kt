81441564680ced654eeba3995d5ebe037005dd26
Zdeněk Kořán <zkoran@gmail.com>
2022-12-17 12:13:38 +0100

    enable StrictMode for fragments

--- app/src/main/java/eu/zkkn/android/disruptions/MainApplication.kt
+++ app/src/main/java/eu/zkkn/android/disruptions/MainApplication.kt
@@ -1,6 +1,8 @@
 package eu.zkkn.android.disruptions
 
 import android.app.Application
+import androidx.fragment.app.strictmode.FragmentStrictMode
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 import eu.zkkn.android.disruptions.utils.Analytics
 
 
@@ -9,6 +11,31 @@ open class MainApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         Analytics.init(this)
+        enableFragmentsStrictMode()
     }
 
+    private fun enableFragmentsStrictMode() {
+        // https://developer.android.com/guide/fragments/debugging#strictmode
+        FragmentStrictMode.defaultPolicy =
+            FragmentStrictMode.Policy.Builder()
+                .detectFragmentReuse()
+                //TODO: .detectFragmentTagUsage()
+                .detectRetainInstanceUsage()
+                .detectSetUserVisibleHint()
+                .detectTargetFragmentUsage()
+                .detectWrongFragmentContainer()
+                .apply {
+                    if (BuildConfig.DEBUG) {
+                        // Fail early on DEBUG builds
+                        penaltyDeath()
+                    } else {
+                        // Log to Crashlytics on RELEASE builds
+                        penaltyListener {
+                            FirebaseCrashlytics.getInstance().recordException(it)
+                        }
+                    }
+                }
+                .build()
+
+    }
 }
